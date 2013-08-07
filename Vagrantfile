708cd804e9daf5a685acba282b836ef057543a8c
Hippie Hacker <chris@hippiehacker.org>
2013-08-07 15:41:28 +0200

    adding rebar + dynamo

--- Vagrantfile
+++ Vagrantfile
@@ -2,6 +2,8 @@
 # vi: set ft=ruby :
 VAGRANTFILE_API_VERSION = "2"
 Vagrant.configure(VAGRANTFILE_API_VERSION) do |config|
+  config.omnibus.chef_version = '11.6.0'
+  config.vm.provision :shell, inline: 'sudo chef-apply /vagrant/recipe.rb'
   config.vm.define :ubuntu do |ubuntu|
     ubuntu.vm.box = "ubuntu_13.04_base"
     ubuntu.vm.box_url = "https://opscode-vm-bento.s3.amazonaws.com/vagrant/opscode_ubuntu-13.04_provisionerless.box"
@@ -10,6 +12,4 @@ Vagrant.configure(VAGRANTFILE_API_VERSION) do |config|
     centos.vm.box = "centos_6.4_base"
     centos.vm.box_url = "https://opscode-vm-bento.s3.amazonaws.com/vagrant/opscode_centos-6.4_provisionerless.box"
   end
-  config.omnibus.chef_version = '11.6.0'
-  config.vm.provision :shell, inline: 'sudo chef-apply /vagrant/recipe.rb'
 end
--- recipe.rb
+++ recipe.rb
@@ -1,63 +1,106 @@
-# what dev tools do we actually need?
-   # autoconf
-   # automake
-   # binutils
-   # bison
-   # flex
-   # gcc
-   # gcc-c++
-   # gettext
-   # libtool
-   # make
-   # patch
-   # pkgconfig
-   # redhat-rpm-config
-   # rpm-build
-   # byacc
-   # git
-   # patchutils
+##### attributes / variables
 
-packages = case node['platform_family']
-           when 'rhel'
-             %w{
-   wxGTK-gl
-   unixODBC
-}
-           when 'debian'
-             %w{
- build-essential
- openjdk-7-jre-headless
- libwxgtk2.8-0
-}
-           end
+elixir_release = 'v0.10.1'
+rebar_release = '2.0.0'
+dynamo_release = 'elixir-0.10.0'
+elixir_dir = '/opt/elixir'
+dynamo_dir = '/opt/dynamo'
 
-# https://www.erlang-solutions.com/downloads/download-erlang-otp
-packages.each do |pkg| # install each of the above
+case node['platform_family']
+when 'rhel'
+  pkg_prov = Chef::Provider::Package::Rpm
+  packages =  %w{
+    git
+    wxGTK-gl
+    unixODBC
+  }  
+  erlpkg = 'package_R16B01_centos664_1371569861/esl-erlang-R16B01-1.x86_64.rpm'
+when 'debian'
+  pkg_prov = Chef::Provider::Package::Dpkg
+  packages = %w{
+    build-essential
+    git-core
+    openjdk-7-jre-headless
+    libwxgtk2.8-0
+  }
+  erlpkg = 'package_R16B01_raring64_1371559180/esl-erlang_16.b.1-1~ubuntu~raring_amd64.deb'
+end
+
+erlpkg_file = '/tmp/esl-erlang.pkgtype'
+##### chef resources
+
+packages.each do |pkg|
   package pkg
 end
 
-erlpkg_url = case node['platform_family']
-            when 'debian'
-              # currently this is for raring only... might need futher logic for 12.04 12.10 etc
-              'https://elearning.erlang-solutions.com/couchdb//rbingen_adapter//package_R16B01_raring64_1371559180/esl-erlang_16.b.1-1~ubuntu~raring_amd64.deb'
-            when 'rhel'
-               # currently targeting centos 6
-              'https://elearning.erlang-solutions.com/couchdb//rbingen_adapter//package_R16B01_centos664_1371569861/esl-erlang-R16B01-1.x86_64.rpm'
-            end
-
-erlpkg_file = '/tmp/esl-erlang.pkgtype'
-
+# From https://www.erlang-solutions.com/downloads/download-erlang-otp
 remote_file erlpkg_file do
-  source erlpkg_url
+  source 'https://elearning.erlang-solutions.com/couchdb//rbingen_adapter//' + erlpkg
 end
 
 package 'esl-erlang' do
   source erlpkg_file
-  provider case node['platform_family']
-            when 'rhel'
-             Chef::Provider::Package::Rpm
-            when 'debian'
-             Chef::Provider::Package::Dpkg
-            end
+  provider pkg_prov
+  not_if 'erl -version 2>&1| grep 5.10.2'
 end
 
+git elixir_dir do
+  repository 'https://github.com/elixir-lang/elixir.git'
+  reference elixir_release
+end
+
+bash 'Compile and Test Elixer' do
+  code <<-EOC
+  make test
+  EOC
+  cwd elixir_dir
+  timeout nil
+  returns [0,2] # does the below help us understand why 2 is returned?
+# Finished in 1.5 seconds (1.2s on load, 0.3s on tests)
+# 93 tests, 0 failures
+# ==> doctest (exunit)
+# STDERR: Killed
+# make: *** [test_doc_test] Error 137
+# ---- End output of "bash"  "/tmp/chef-script20130807-25188-15hc020" ----
+# Ran "bash"  "/tmp/chef-script20130807-25188-15hc020" returned 2
+  creates "#{elixir_dir}/lib/elixir/ebin/elixir.app"
+end
+
+git "#{Chef::Config[:file_cache_path]}/rebar" do
+  repository 'https://github.com/rebar/rebar.git'
+  reference rebar_release
+  notifies :run, 'execute[make rebar]'
+end
+
+execute 'make rebar' do
+  command './bootstrap'
+  action :nothing
+  cwd "#{Chef::Config[:file_cache_path]}/rebar"
+end
+
+link "#{elixir_dir}/bin/rebar" do
+  to "#{Chef::Config[:file_cache_path]}/rebar/rebar"
+end
+
+file '/etc/profile.d/elixier.sh' do
+  content <<-EOE
+  PATH="$PATH::#{elixir_dir}/bin"
+  EOE
+end
+
+git dynamo_dir do
+  repository 'https://github.com/elixir-lang/dynamo.git'
+  reference dynamo_release
+  notifies :run, 'execute[make dynamo]'
+end
+
+execute 'make dynamo' do
+  # warning... this uses git:// remotes http_proxies won't work
+  # || true because many tests fail.. make make w/o tests?
+  command 'mix do deps.get, test || true'
+  environment ({
+    'MIX_ENV' => 'test'
+  })
+  action :nothing
+  cwd dynamo_dir
+end
